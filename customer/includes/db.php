6bf80e30b8177e837ed2e27783d3fe4d4131b47b
Павел Смирнов <Pasha@MacBook-Air-Pavel.local>
2019-06-16 11:32:38 +0300

    Initial commit: Add E-Commerce Shop

---
+++ customer/includes/db.php
@@ -0,0 +1,7 @@
+<?php 
+// After uploading to online server, change this connection accordingly
+$con = mysqli_connect("localhost","root","","e-commerce");
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+?>
